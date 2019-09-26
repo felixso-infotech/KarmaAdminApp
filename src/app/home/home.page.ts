c5702c4ae4f9c8d11bf3b7c1934447c31864d02d
AnjaliLxi <anjali.chandran@lxisoft.com>
2019-09-26 14:05:22 +0530

    added drop down and checkbox KAR-208 KAR-206 KAR-200

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,11 @@ export class HomePage {
 
   constructor() {}
 
+     public form = [
+          { val: 'Pepperoni', isChecked: true },
+          { val: 'Sausage', isChecked: false },
+          { val: 'Mushroom', isChecked: false }
+        ];
+
+
 }
